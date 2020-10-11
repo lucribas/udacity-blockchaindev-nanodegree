19ee4903447bce7f9d82a41adc5884cf0f1000e7
Luciano da Silva Ribas <lucribas@users.noreply.github.com>
2020-10-11 19:22:33 -0300

    saving work

--- project_2_CryptoStar_Dapp_on_Ethereum/truffle-config.js
+++ project_2_CryptoStar_Dapp_on_Ethereum/truffle-config.js
@@ -90,17 +90,16 @@ module.exports = {
 	// network_id: 2111,   // This network is yours, in the cloud.
 	// production: true    // Treats this network as if it was a public net. (default: false)
 	// }
-},
+
 
 	// Set default mocha options here, use special reporters etc.
-	mocha: {
-	// timeout: 100000
-},
 
-// Configure your compilers
-compilers: {
-	solc: {
-		version: "0.6.12",    // Fetch exact version from solc-bin (default: truffle's version)
+	// timeout: 100000
+
+	// Configure your compilers
+	compilers: {
+		solc: {
+			version: "0.6.12",    // Fetch exact version from solc-bin (default: truffle's version)
 			// docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
 			// settings: {          // See the solidity docs for advice about optimization and evmVersion
 			//  optimizer: {
@@ -110,5 +109,5 @@ compilers: {
 			//  evmVersion: "byzantium"
 			// }
 		}
-}
+	}
 }
